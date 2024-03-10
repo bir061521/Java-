ef137dd66a2645fc55838739b0ad3b0de9328a61
KoMuhly <152574864+KoMuhly@users.noreply.github.com>
2024-03-11 07:30:02 +0900

    클래스와 인스턴스 클래스 변수와 인스턴스 변수

--- src/_06oop/Ex6_4.java
+++ src/_06oop/Ex6_4.java
@@ -1,2 +1,29 @@
-package _06oop;public class Ex6_4 {
+package _06oop;
+
+public class Ex6_4 {
+}
+//객체배열
+class TvTest4{
+    public static void main(String[] args) {
+        Tv[]tvArr = new Tv[3];
+
+        for(int i = 0; i < tvArr.length; i++){
+            tvArr[i] = new Tv();
+            tvArr[i].channel = i + 10;
+        }
+        for(int i = 0; i < tvArr.length; i++){
+            tvArr[i].channelUp();
+            System.out.printf("tvArr[%d].channel=%d%n",i,tvArr[i].channel);
+        }
+    }
+}
+
+class Tv4{
+    String color;
+    boolean power;
+    int channel;
+
+    void power(){power = !power;}
+    void channelUp(){++channel;}
+    void channelDown(){--channel;}
 }
